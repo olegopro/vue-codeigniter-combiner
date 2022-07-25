a57c2cde34f987a29c9b64b6f464b5b4ee3af2d8
olegopro <evilgazz@gmail.com>
2022-07-25 22:40:43 +0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'@vue/standard'
+	],
+	parserOptions: {
+		parser: '@babel/eslint-parser'
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'indent': 'off',
+		'no-tabs': 'off',
+		'space-before-function-paren': 'off',
+		'no-trailing-spaces': 'off',
+		'vue/require-explicit-emits': 'error'
+	}
+
 }
